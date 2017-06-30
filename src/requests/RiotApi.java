5e27c86f24ded5428c553137905a9b4e437a8730
Stephan Strate <development@famstrate.com>
2017-06-30 09:40:17 +0200

    initialize independent riot api

---
+++ src/requests/RiotApi.java
@@ -0,0 +1,11 @@
+package requests;
+
+public class RiotApi {
+    private String key;
+    private String region;
+
+    public RiotApi (String key, String region) {
+        this.key = key;
+        this.region = region;
+    }
+}
