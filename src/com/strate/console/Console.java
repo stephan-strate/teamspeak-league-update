4be6791428bc211f466d60dae0e09d7756022828
Stephan Strate <development@famstrate.com>
2017-11-22 11:02:33 +0100

    wip

--- src/com/strate/console/Console.java
+++ src/com/strate/console/Console.java
@@ -1,58 +1,24 @@
 package com.strate.console;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-abstract class Console {
+class Console {
 
     boolean actvie = false;
     ArrayList<Method> methods = new ArrayList<>();
-    private Class obj = getClass();
-    private Object o;
 
     Console () {
-        for (Method method : obj.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(com.strate.console.Method.class)) {
+        for (Method method : getClass().getDeclaredMethods()) {
+            if (method.isAnnotationPresent(com.strate.console.Command.class)) {
                 // get all methods that have annotation
                 methods.add(method);
             }
         }
     }
 
-    private void process () {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (actvie) {
-            try {
-                String command = br.readLine();
-                String[] parts = command.split(" ");
-                if (parts.length > 0) {
-                    String endpoint = parts[0];
-                    String[] args = Arrays.copyOfRange(parts, 1, parts.length);
+    protected void process () {
 
-                    for (Method method : methods) {
-                        if (method.getName().equals(endpoint)) {
-                            o = obj.newInstance();
-                            Object[] params = new Object[1];
-                            params[0] = args;
-                            method.invoke(o, params);
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("");
-            } catch (IllegalAccessException e) {
-                System.out.println("");
-            } catch (InvocationTargetException e) {
-                System.out.println("");
-            } catch (InstantiationException e) {
-
-            }
-        }
     }
 
     public void start () {
@@ -62,5 +28,6 @@ abstract class Console {
 
     public void end () {
         actvie = false;
+        System.exit(0);
     }
 }
--- src/com/strate/console/DefaultConsole.java
+++ src/com/strate/console/DefaultConsole.java
@@ -1,10 +1,50 @@
 package com.strate.console;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class DefaultConsole extends Console {
 
-    // final console class
+    private static final Console obj = new DefaultConsole();
 
-    @Method
+    @Override
+    protected void process () {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (actvie) {
+            try {
+                String command = br.readLine();
+                String[] parts = command.split(" ");
+                if (parts.length > 0) {
+                    String endpoint = parts[0];
+                    String[] args = Arrays.copyOfRange(parts, 1, parts.length);
+
+                    for (Method method : methods) {
+                        if (method.getName().equals(endpoint)) {
+                            Object[] params = new Object[1];
+                            params[0] = args;
+                            method.invoke(getInstance(), params);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("");
+            } catch (IllegalAccessException e) {
+                System.out.println("");
+            } catch (InvocationTargetException e) {
+                System.out.println("");
+            }
+        }
+    }
+
+    public Object getInstance () {
+        return obj;
+    }
+
+    @Command
     protected void update (String[] args) {
         // do something when user calls function
         for (int i = 0; i < args.length; i++) {
@@ -12,9 +52,9 @@ public class DefaultConsole extends Console {
         }
     }
 
-    @Method
+    @Command
     protected void exit (String[] args) {
         System.out.println("Exit program");
-        System.exit(0);
+        end();
     }
 }
