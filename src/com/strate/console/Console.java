4dcd6f58cad3d24bbbc3554f53a52ee7e140587b
Stephan Strate <development@famstrate.com>
2017-12-10 13:41:11 +0100

    added singleton pattern to console

--- src/com/strate/console/Console.java
+++ src/com/strate/console/Console.java
@@ -16,30 +16,44 @@ abstract class Console {
     private Object o;
 
     Console () {
+        /* @TODO: Methods get iterate one time to much, when creating the console */
         for (Method method : obj.getDeclaredMethods()) {
             if (method.isAnnotationPresent(com.strate.console.Method.class)) {
                 // get all methods that have annotation
                 methods.add(method);
+                System.out.println(method);
             }
         }
     }
 
     private void process () {
+        // init reader
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (actvie) {
             try {
+                // read line
                 String command = br.readLine();
+                // split inputs from eg. "update database test" to
+                // ["update", "database", "test"]
                 String[] parts = command.split(" ");
+                // if command is available
                 if (parts.length > 0) {
+                    // get command/endpoint
                     String endpoint = parts[0];
+                    // get the attributes
                     String[] args = Arrays.copyOfRange(parts, 1, parts.length);
 
+                    // iterate all methods
                     for (Method method : methods) {
+                        // check if any method has the name of endpoint
                         if (method.getName().equals(endpoint)) {
-                            o = obj.newInstance();
+                            // get singleton instance
+                            Object temp = getInstance();
+                            // prepare arguments for method
                             Object[] params = new Object[1];
                             params[0] = args;
-                            method.invoke(o, params);
+                            // invoke method
+                            method.invoke(temp, params);
                         }
                     }
                 }
@@ -49,9 +63,23 @@ abstract class Console {
                 System.out.println("");
             } catch (InvocationTargetException e) {
                 System.out.println("");
+            }
+        }
+    }
+
+    private Object getInstance () {
+        if (o == null) {
+            try {
+                o = obj.newInstance();
             } catch (InstantiationException e) {
 
+            } catch (IllegalAccessException e) {
+
             }
+
+            return o;
+        } else {
+            return o;
         }
     }
 
@@ -61,6 +89,12 @@ abstract class Console {
     }
 
     public void end () {
+        actvie = false;
+    }
+
+    @com.strate.console.Method
+    public void exit (String[] args) {
+        System.out.println("Exit program");
         actvie = false;
     }
 }
--- src/com/strate/console/DefaultConsole.java
+++ src/com/strate/console/DefaultConsole.java
@@ -2,19 +2,11 @@ package com.strate.console;
 
 public class DefaultConsole extends Console {
 
-    // final console class
-
     @Method
     protected void update (String[] args) {
         // do something when user calls function
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
-    }
-
-    @Method
-    protected void exit (String[] args) {
-        System.out.println("Exit program");
-        System.exit(0);
     }
 }
