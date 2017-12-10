836903f59066b00bb32fb62b5410d8427aca054e
Stephan Strate <development@famstrate.com>
2017-12-10 18:02:41 +0100

    access rights, improved commenting

--- src/com/strate/console/DefaultConsole.java
+++ src/com/strate/console/DefaultConsole.java
@@ -1,7 +1,18 @@
 package com.strate.console;
 
+/**
+ * <p>Default console application, that's controlling
+ * teamspeak-league-update bot.</p>
+ * @author Stephan Strate
+ * @since 3.0.0
+ */
 public class DefaultConsole extends Console {
 
+    /**
+     * <p>Dummy method. Output all arguments.</p>
+     * @param args  arguments
+     * @since 3.0.0
+     */
     @Method
     protected void update (String[] args) {
         // do something when user calls function
--- src/com/strate/console/Method.java
+++ src/com/strate/console/Method.java
@@ -5,8 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * <p>Method annotation to declare console methods
+ * for {@see Console}.</p>
+ * @author Stephan Strate
+ * @since 3.0.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface Method {
+@interface Method {
 
 }
