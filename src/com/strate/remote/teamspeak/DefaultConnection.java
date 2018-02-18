4884ff0fc916204ca990a5eed2e13050a1425d34
Stephan Strate <development@famstrate.com>
2018-02-18 13:57:56 +0100

    added database to store users in

--- src/com/strate/remote/teamspeak/DefaultConnection.java
+++ src/com/strate/remote/teamspeak/DefaultConnection.java
@@ -12,6 +12,7 @@ import com.strate.Init;
 import com.strate.remote.riot.Api;
 import com.strate.remote.riot.constants.League;
 import com.strate.remote.riot.constants.Region;
+import com.strate.sql.tables.events.Users;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -70,61 +71,67 @@ public class DefaultConnection extends Connection {
      * @since 3.0.0
      */
     private void clientJoin (ClientJoinEvent e) {
-        // current riot game account
-        long accountId = 0;
-        Api api = new Api(Init.s.getPropertie("apikey"), Region.getRegionByShortcut(Init.s.getPropertie("region")));
-        League account = api.getLeagueById(accountId);
+        System.out.println("[" + new Date().toString() + "][tlu] '" + e.getClientNickname() + "' joined the server.");
+        long accountId = new Users().getSummonerId(e.getUniqueClientIdentifier());
 
-        // old teamspeak league
-        int clientId = e.getClientId();
-        ClientInfo clientInfo = getTs3Api().getClientInfo(clientId);
-        League teamspeak = League.UNRANKED;
+        if (accountId != 0) {
+            // current riot game account
+            Api api = new Api(Init.s.getPropertie("apikey"), Region.getRegionByShortcut(Init.s.getPropertie("region")));
+            League account = api.getLeagueById(accountId);
 
-        // get all server groups with id
-        HashMap<League, String> serverGroupsByLeague = new HashMap<>();
-        HashMap<String, League> serverGroupsById = new HashMap<>();
-        for (League league : League.getAllLeagues()) {
-            String id = Init.s.getPropertie(league.getName());
-            serverGroupsByLeague.put(league, id);
-            serverGroupsById.put(id, league);
-        }
+            // old teamspeak league
+            int clientId = e.getClientId();
+            ClientInfo clientInfo = getTs3Api().getClientInfo(clientId);
+            League teamspeak = League.UNRANKED;
 
-        // get all assigned league server groups
-        List<League> teamspeakLeagues = new LinkedList<>();
-        for (int group : clientInfo.getServerGroups()) {
-            if (serverGroupsById.containsKey(group + "")) {
-                teamspeakLeagues.add(serverGroupsById.get(group + ""));
+            // get all server groups with id
+            HashMap<League, String> serverGroupsByLeague = new HashMap<>();
+            HashMap<String, League> serverGroupsById = new HashMap<>();
+            for (League league : League.getAllLeagues()) {
+                String id = Init.s.getPropertie(league.getName());
+                serverGroupsByLeague.put(league, id);
+                serverGroupsById.put(id, league);
             }
-        }
 
-        // get the highest rank
-        for (League league : teamspeakLeagues) {
-            if (league.compare(teamspeak) > 0) {
-                teamspeak = league;
-            }
-        }
-
-        int invoker = e.getClientDatabaseId();
-        if (!teamspeak.equals(account)) {
-            // account league is higher
-            if (teamspeakLeagues.size() > 1) {
-                // more than one league is assigned, remove all
-                for (League league : teamspeakLeagues) {
-                    getTs3Api().removeClientFromServerGroup(Integer.parseInt(serverGroupsByLeague.get(league)), invoker);
+            // get all assigned league server groups
+            List<League> teamspeakLeagues = new LinkedList<>();
+            for (int group : clientInfo.getServerGroups()) {
+                if (serverGroupsById.containsKey(group + "")) {
+                    teamspeakLeagues.add(serverGroupsById.get(group + ""));
                 }
-            } else {
-                // remove the single league
-                getTs3Api().removeClientFromServerGroup(Integer.parseInt(serverGroupsByLeague.get(teamspeak)), invoker);
             }
 
-            // assign new league
-            getTs3Api().addClientToServerGroup(Integer.parseInt(serverGroupsByLeague.get(account)), invoker);
-        } else if (teamspeakLeagues.size() > 1) {
-            // account league is not higher, but more than one league is assigned
+            // get the highest rank
             for (League league : teamspeakLeagues) {
-                // remove all leagues that are not the highest
-                if (!league.equals(teamspeak)) {
-                    getTs3Api().removeClientFromServerGroup(Integer.parseInt(serverGroupsByLeague.get(league)), invoker);
+                if (league.compare(teamspeak) > 0) {
+                    teamspeak = league;
+                }
+            }
+
+            int invoker = e.getClientDatabaseId();
+            if (!teamspeak.equals(account)) {
+                System.out.println("[" + new Date().toString() + "][tlu] Assigning new league " + account.getName() + " and removing " + teamspeak.getName() + ".");
+
+                // account league is higher
+                if (teamspeakLeagues.size() > 1) {
+                    // more than one league is assigned, remove all
+                    for (League league : teamspeakLeagues) {
+                        getTs3Api().removeClientFromServerGroup(Integer.parseInt(serverGroupsByLeague.get(league)), invoker);
+                    }
+                } else {
+                    // remove the single league
+                    getTs3Api().removeClientFromServerGroup(Integer.parseInt(serverGroupsByLeague.get(teamspeak)), invoker);
+                }
+
+                // assign new league
+                getTs3Api().addClientToServerGroup(Integer.parseInt(serverGroupsByLeague.get(account)), invoker);
+            } else if (teamspeakLeagues.size() > 1) {
+                // account league is not higher, but more than one league is assigned
+                for (League league : teamspeakLeagues) {
+                    // remove all leagues that are not the highest
+                    if (!league.equals(teamspeak)) {
+                        getTs3Api().removeClientFromServerGroup(Integer.parseInt(serverGroupsByLeague.get(league)), invoker);
+                    }
                 }
             }
         }
@@ -133,6 +140,7 @@ public class DefaultConnection extends Connection {
     /**
      * <p></p>
      * @param e
+     * @param message
      * @since 3.0.0
      */
     private void textMessage (TextMessageEvent e, String message) {
@@ -142,11 +150,11 @@ public class DefaultConnection extends Connection {
 
         } else if (message.equals("!help")) {
             // a bot can't help
-            getTs3Api().sendChannelMessage("");
+            getTs3Api().sendChannelMessage("I can't help you.");
 
         } else if (message.equals("!name"))  {
             // short explanation for the name system
-            getTs3Api().sendChannelMessage("");
+            getTs3Api().sendChannelMessage("Usage: !name [Your League of Legends name]");
 
         } else if (message.startsWith("!name ")) {
             System.out.println("Checking league name...");
@@ -154,15 +162,18 @@ public class DefaultConnection extends Connection {
             String leagueName = message.substring(6);
 
             // check if name is spelled right, or riot server is down
-            if (/* Mysql.editDatabaseIdentifier(leagueName, e.getInvokerUniqueId(), RiotApi.getIdBySummonerName(leagueName))*/true) {
+            Users users = new Users();
+            if (users.assign(e.getInvokerUniqueId(), e.getInvokerName(), new Api(Init.s.getPropertie("apikey"),
+                    Region.getRegionByShortcut(Init.s.getPropertie("region"))).getIdBySummonerName(leagueName), leagueName)) {
+
                 getTs3Api().sendChannelMessage("[b]" + e.getInvokerName() +
-                        "[/b] " + "");
+                        "[/b] " + "added to database. Reconnect to get your league assigned.");
             } else {
-                getTs3Api().sendChannelMessage("");
+                getTs3Api().sendChannelMessage("Error while adding your League of Legends name, please try again later.");
             }
         } else if (message.startsWith("!")) {
             // after all equals, error message
-            getTs3Api().sendChannelMessage("");
+            getTs3Api().sendChannelMessage("Command not found.");
         }
     }
 
--- src/com/strate/sql/tables/events/Users.java
+++ src/com/strate/sql/tables/events/Users.java
@@ -3,13 +3,73 @@ package com.strate.sql.tables.events;
 import com.strate.sql.Table;
 import com.strate.sql.databases.Events;
 
+import java.sql.*;
+
 public class Users extends Table {
 
     public Users () {
-        super("users", new Events(), "");
+        super("users", new Events(),
+                "CREATE TABLE IF NOT EXISTS users (\n" +
+                        "id integer PRIMARY KEY,\n" +
+                        "teamspeak_identifier text NOT NULL,\n" +
+                        "teamspeak_nickname text NOT NULL,\n" +
+                        "league_identifier long NOT NULL,\n" +
+                        "league_username text NOT NULL,\n" +
+                        "added text NOT NULL,\n" +
+                        "last_modified text NOT NULL\n" +
+                        ");");
+    }
+
+    public boolean assign (String teamspeakId, String teamspeakName, long leagueId, String leagueName) {
+        if (leagueId != -1) {
+            String sql = "SELECT * FROM users WHERE teamspeak_identifier = '" + teamspeakId + "'";
+            try {
+                Connection connection = getDatabase().getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(sql);
+
+                if (resultSet.next()) {
+                    sql = "UPDATE users SET teamspeak_nickname = '" + teamspeakName + "', league_identifier = " + leagueId + ", league_username = '" + leagueName + "' " +
+                            "WHERE teamspeak_identifier = '" + teamspeakId + "'";
+                    statement.execute(sql);
+                } else {
+                    sql = "INSERT INTO users(teamspeak_identifier, teamspeak_nickname, league_identifier, league_username, added, last_modified) " +
+                            "VALUES(?, ?, ?, ?, datetime('now', 'localtime'), datetime('now', 'localtime'))";
+
+                    PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                    preparedStatement.setString(1, teamspeakId);
+                    preparedStatement.setString(2, teamspeakName);
+                    preparedStatement.setLong(3, leagueId);
+                    preparedStatement.setString(4, leagueName);
+
+                    preparedStatement.executeUpdate();
+                }
+
+                getDatabase().closeConnection();
+                return true;
+            } catch (SQLException e) {
+                // handle errors
+                return false;
+            }
+        }
+
+        return false;
     }
 
     public long getSummonerId (String uid) {
+        String sql = "SELECT * FROM users WHERE teamspeak_identifier = '" + uid + "'";
+        try {
+            Connection connection = getDatabase().getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            if (resultSet.next()) {
+                return resultSet.getLong("league_identifier");
+            }
+        } catch (SQLException e) {
+            return 0;
+        }
+
         return 0;
     }
 }
